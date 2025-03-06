60acd2070dcc242197ccf4afdd9e1e0cf921b369
MarcsWeb04 <marcusfavernay@gmail.com>
2025-03-06 11:14:15 +0100

    ajout du formulaire de connexion, de toute les requettes api user pour react dans le fichier apiRequest

--- frontend/src/pages/Connexion.jsx
+++ frontend/src/pages/Connexion.jsx
@@ -1,8 +1,52 @@
+import { useState } from "react";
+import { logIn } from "../service/requestApi";
+import backgroundImage from "../image/background/background-form.webp";
+
+// css
+import "./Connexion.css";
+
 function Connexion(){
+    const [formData,setData] = useState([]);
+    const [email,setEmail] = useState([]);
+    const [password,setPassword] = useState([]);
+
+    const handleEmail = (element) => {
+        const email = element.target.value;
+        setEmail(email);
+    };
+
+    const handlePassword = (element) => {
+        const password = element.target.value;
+        setPassword(password);
+    }
+    
+    const handleSubmit = (event) => {
+
+      event.preventDefault();
+      const data = [email,password];
+      setData(data);
+
+      logIn(formData);
+    };
+
     return(
     <>
-        <div>
-            <h1>Connexion</h1>
+        <div className="container">
+            <img src={backgroundImage} alt="" />
+            <div className="content">
+                <h1>MealMates</h1>
+                <form action="#" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" placeholder="Test@email.com" onChange={handleEmail}/>
+                    </div>
+                    <div>
+                        <label htmlFor="password">Mot de Passe:</label>
+                        <input type="password" name="password" placeholder="password" onChange={handlePassword}/>
+                    </div>
+                    <button type="submit">valider</button>
+                </form>
+            </div>
         </div>
     </>
     )
---
+++ frontend/src/service/requestApi.jsx
@@ -0,0 +1,167 @@
+const token = '';
+
+// User
+
+// login user
+export async function logIn(data) {
+    try{
+        const request = await fetch ("http://localhost:8000/api/login",{
+            method: "GET",
+            headers: {Authorization: `Bearer ${token}`},
+            body:{
+                "username" : data[email],
+                "password" : data[password],
+            },
+        });
+
+        if(!request.ok){
+            const errorDetails = await request.json();
+            console.error(
+                `Erreur API(Statut ${request.status});`,
+                errorDetails
+            );
+            
+            throw new Error(
+                `Erreur lors de la récupération de l'utilisateur : ${errorDetails.error.message}`
+            );
+        }
+
+        return await request.json();
+
+    }catch(error){
+        console.error("erreur api :", error);
+        throw error;
+    }
+}
+
+// recuperer un user
+export async function getUser(id) {
+    try{
+        const request = await fetch ("https://127.0.0.1:8000/api/user/get/",{
+            method: "POST",
+            headers: {Authorization: `Bearer ${token}`},
+            body:{
+                "id": id,
+            }
+        });
+
+        if(!request.ok){
+            const errorDetails = await request.json();
+            console.error(
+                `Erreur API(Statut ${request.status});`,
+                errorDetails
+            );
+            
+            throw new Error(
+                `Erreur lors de la récupération de l'utilisateur : ${errorDetails.error.message}`
+            );
+        }
+
+        return await request.json();
+
+    }catch(error){
+        console.error("erreur api :", error);
+        throw error;
+    }
+}
+
+// creer un utilisateur
+export async function newUser(data) {
+    try{
+        const request = await fetch ("https://127.0.0.1:8000/api/user/new/",{
+            method: "GET",
+            headers: {Authorization: `Bearer ${token}`},
+            body : {
+                "email" : data[email],
+                "password" : data[password],
+                "password_confirm" : data[password],
+                "name" : data[nameUser],
+                "surname" : data[surname],   
+            },
+        });
+
+        if(!request.ok){
+            const errorDetails = await request.json();
+            console.error(
+                `Erreur API(Statut ${request.status});`,
+                errorDetails
+            );
+            
+            throw new Error(
+                `Erreur lors de la récupération de l'utilisateur : ${errorDetails.error.message}`
+            );
+        }
+        
+        return await request.json();
+
+    }catch(error){
+        console.error("erreur api :", error);
+        throw error;
+    }
+}
+
+// modifier un user
+export async function editUser(data) {
+    try{
+        const request = await fetch ("https://127.0.0.1:8000/api/user/edit/",{
+            method: "GET",
+            headers: {Authorization: `Bearer ${token}`},
+            body : {
+                "email" : data[email],
+                "password" : data[password],
+                "password_confirm" : data[password],
+                "name" : data[nameUser],
+                "surname" : data[surname],   
+            },
+        });
+
+        if(!request.ok){
+            const errorDetails = await request.json();
+            console.error(
+                `Erreur API(Statut ${request.status});`,
+                errorDetails
+            );
+            
+            throw new Error(
+                `Erreur lors de la récupération de l'utilisateur : ${errorDetails.error.message}`
+            );
+        }
+        
+        return await request.json();
+
+    }catch(error){
+        console.error("erreur api :", error);
+        throw error;
+    }
+}
+
+// supprimer un user
+export async function deleteUser(id) {
+    try{
+        const request = await fetch ("https://127.0.0.1:8000/api/user/delete/",{
+            method: "GET",
+            headers: {Authorization: `Bearer ${token}`},
+            body : { 
+                "id":id 
+            },
+        });
+
+        if(!request.ok){
+            const errorDetails = await request.json();
+            console.error(
+                `Erreur API(Statut ${request.status});`,
+                errorDetails
+            );
+            
+            throw new Error(
+                `Erreur lors de la récupération de l'utilisateur : ${errorDetails.error.message}`
+            );
+        }
+        
+        return await request.json();
+
+    }catch(error){
+        console.error("erreur api :", error);
+        throw error;
+    }
+}
