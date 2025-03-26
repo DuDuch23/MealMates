f15b16f8973a38bc0fb1c3039875a3fea234a1e3
MarcsWeb04 <marcusfavernay@gmail.com>
2025-03-26 20:08:32 +0100

    update de l'us-4 création du front mais api et sans les avis

--- frontend/src/pages/UserProfile/UserProfile.jsx
+++ frontend/src/pages/UserProfile/UserProfile.jsx
@@ -5,18 +5,38 @@ import { getUser } from "../../service/requestApi";
 import './UserProfile.css';
 
 function UserProfile(){
-    const {id} = useParams();
-    const [dataUser,setDataUser] = useState([]);
+    // const {id} = useParams();
+    // const [dataUser,setDataUser] = useState([]);
 
-    useEffect(()=>{
-        getUser({id});
-    },
-    [{id}]);
+    // useEffect(()=>{
+    //     getUser({id});
+    // },
+    // [{id}]);
 
     return(
         <>
-            <div>
-                <h2>Mon profil</h2>
+            <div id="page-user">
+                <div id="identy-user">
+                    <img id="image-user"  alt="user image" />
+                    <ul>
+                        <li>Nom de l'utilisateur</li>
+                        <li>Adresse de l'utilisateur</li>
+                    </ul>
+                </div>
+
+                <ul id="params-user">
+                    <li>
+                        Préférence de l'utilisateur:<br/>
+                    </li>
+                    <li>
+                        Disponibilité de l'utilisateur:<br/>
+                    </li>
+                </ul>
+
+                {/* creer un composant pour le slider avis afin de pouvoir gerer le fait que certain utilisateur n'ont pas d'avis */}
+                <div id="slider-avis">
+
+                </div>
             </div>
         </>
     );
