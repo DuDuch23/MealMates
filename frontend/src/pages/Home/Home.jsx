9b40cafbe9d066561822881a75fcd73bf54432e3
snekye <dimitrigranit22@gmail.com>
2025-04-03 10:50:19 +0200

    landing page bottom

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './Home.css';
-import bg1 from '../../assets/logo-mealmates.png';
+import logo from '../../assets/logo-mealmates.png';
+import map from '../../assets/landing-map.png';
 
 function App() {
     return (
         <section className="landing">
             <section className="header">
                 <div className="header-left">
-                    <img src={bg1}></img>
+                    <img src={logo}></img>
                     <h1>MealMates</h1>
                 </div>
                 <div className="header-right">
@@ -18,9 +19,21 @@ function App() {
             <section className="top">
                 <h1>Et si on mangait moins cher, plus respectueux de la planète?</h1>
             </section>
+            <section className="inbetween">
+                <p>Luttez contre le gaspillage en achetant à d'autres particuliers, et proposez vos propres surplus alimentaires.</p>
+            </section>
             <section className="bottom">
-                <div className="left"></div>
-                <div className="right"></div>
+                <div className="left">
+                    <div className="circle"></div>
+                    <img src={map}></img>
+                </div>
+                <div className="right">
+                    <h1>Obtenez des offres locales, qui suivent vos offres alimentaires</h1>
+                    <button>M'inscrire maintenant</button>
+                    <div className="footer">
+                        <p>© Mealmates 2025</p>
+                    </div>
+                </div>
             </section>
         </section>
     );
