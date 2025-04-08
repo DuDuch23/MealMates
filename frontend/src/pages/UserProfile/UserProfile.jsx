d24bb6fe7ecf33be95a1fb4725cd72a704b989f7
MarcsWeb04 <marcusfavernay@gmail.com>
2025-04-08 15:33:38 +0200

    ajout des icons des utilisateurs + refontes des users

--- frontend/src/pages/UserProfile/UserProfile.jsx
+++ frontend/src/pages/UserProfile/UserProfile.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
-import { useParams } from 'react-router';
+import { useParams,Link } from 'react-router';
 import { getUser } from "../../service/requestApi";
-
+import IconUser from "../../components/IconUser.jsx/iconUser";
+import Header from "../../components/Header/Header";
 import './UserProfile.css';
 
 function UserProfile() {
@@ -38,6 +39,26 @@ function UserProfile() {
                 <img src="/img/logo-mealmates.png" alt="logo mealmates" />
                 <h2>MealMates</h2>
             </nav>
+            <IconUser id={1}/>
+            <div className="content-user">
+                <div className="container-link">
+                    <Link to="/">Mes informations</Link>
+                    <span></span>
+                    <Link to="/">MealCard</Link>
+                    <span></span>
+                    <Link to="/">Modifier mon compte</Link>
+                </div>
+                <div className="container-info-user">
+                    <div className="basics-elements">
+                        <div><p>Mon nom</p></div>
+                        <div><p>Mon Prénom</p></div>
+                        <div><p>Mon adresse email</p></div>
+                    </div>
+                    <div className="preferences">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     </>);
 }
