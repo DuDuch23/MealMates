aff8b5940f3de0d266dc245a3e1d8f9eb0cc689c
MarcsWeb04 <marcusfavernay@gmail.com>
2025-04-13 21:49:11 +0200

    creation de la branch sans la logique front

---
+++ frontend/src/components/SectionOffer/SectionOffer.jsx
@@ -0,0 +1,16 @@
+export function SectionOffer({name}){
+    return(
+    <>
+    <div className="section">
+        <h2>{name} :</h2>
+        <div>
+            <div>
+                <img src="#" alt="#" />
+                <h4>Offer</h4>
+                <p>name User</p>
+            </div>
+        </div>
+    </div>
+    </>
+    );
+}
---
+++ frontend/src/pages/Offer/Offer.jsx
@@ -0,0 +1,48 @@
+import { useState,useEffect } from "react";
+
+import {Link} from "react-router";
+import "./offer.Css";
+
+function Offer(){
+    return(<>
+    <nav>
+        <ul className="container-pre-offer">
+            <li>
+                <img src="/img/offre/sushi.png" alt="icon de sushi" />
+                <p>Sushi</p>
+            </li>
+            <li>
+                <img src="/img/offre/burger.png" alt="" />
+                <p>Burger</p>
+            </li>
+            <li>
+                <img src="/img/offre/tarte.png" alt="" />
+                <p>Tarte</p>
+            </li>
+            <li>
+                <img src="/img/offre/produits-frais.png" alt="" />
+                <p>Produit Frais</p>
+            </li>
+            <li>
+                <img src="/img/offre/derniere-chance.png" alt="" />
+                <p>Derniére Chance</p>
+            </li>
+        </ul>
+        <ul className="container-offer">
+            <li>Rechercher</li>
+            <li>Vegan</li>
+            <li>Hight Protein</li>
+            <li>Halal</li>
+            <li>Low Carb</li>
+        </ul>
+        <a href="#" id="new-offer">Ajouter Une Offre</a>
+    </nav>
+    <div className="container-section">
+        
+    </div>
+
+    </>
+    );
+}
+
+export default Offer;
